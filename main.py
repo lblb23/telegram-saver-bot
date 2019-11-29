67509d1c6493e64206f632bb6c9294606d4c6127
Lev Bulygin <buliginleo95@gmail.com>
2019-11-29 23:50:03 +0800

    -Remove vk and facebook downloaders

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 # -*- coding: utf-8 -*-
 import logging
-from telegram import InputMediaPhoto, InputMediaVideo, InlineKeyboardButton, InlineKeyboardMarkup
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackQueryHandler
-from utils import get_insta_links, check_instagram, check_facebook, \
-    download_fb_video, check_vkontakte, get_vk_resolutions, download_vk_video, \
-    check_youtube, get_youtube_resolutions, download_yt_video, get_vk_link_by_res, get_yt_link_by_res
 import os
-from shutil import rmtree
-from tinydb import TinyDB, Query
 import time
+from shutil import rmtree
+
 import yaml
+from telegram import InputMediaPhoto, InputMediaVideo, InlineKeyboardButton, \
+    InlineKeyboardMarkup
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, \
+    CallbackQueryHandler
+from tinydb import TinyDB, Query
+
+from utils import get_insta_links, check_instagram, check_youtube, \
+    get_youtube_resolutions, download_yt_video, get_yt_link_by_res
 
 with open('config.yml') as file:
     config = yaml.load(file, Loader=yaml.FullLoader)
@@ -18,9 +21,10 @@ TOKEN = config['TOKEN']
 
 update_id = None
 
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                    level=logging.INFO,
-                    filename='.log')
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO,
+    filename='.log')
 
 logger = logging.getLogger(__name__)
 
@@ -70,22 +74,21 @@ def button(update, context):
     social_network, video_id, res, size = query.data.split('--')
 
     if float(size) > 50:
-        if social_network == 'vk':
-            url = get_vk_link_by_res(video_id, res)
-        elif social_network == 'yt':
+        if social_network == 'yt':
             url = get_yt_link_by_res(video_id, res)
-        context.bot.send_message(chat_id=query.from_user.id, parse_mode='Markdown',
+        context.bot.send_message(chat_id=query.from_user.id,
+                                 parse_mode='Markdown',
                                  text="Telegram allows to send video only up to 50 mb, so we can only give a direct link: [link](" + url + ")")
     else:
-        context.bot.send_message(query.from_user.id, "Your video is loading ...")
+        context.bot.send_message(query.from_user.id,
+                                 "Your video is loading ...")
 
-        if 'vk' in social_network:
-            flag, path = download_vk_video(video_id, res)
-        elif 'yt' in social_network:
+        if 'yt' in social_network:
             flag, path = download_yt_video(video_id, res)
 
         video_file = open(path, 'rb')
-        context.bot.send_video(update.callback_query.from_user.id, video_file, timeout=200)
+        context.bot.send_video(update.callback_query.from_user.id, video_file,
+                               timeout=200)
 
         rmtree(os.path.join("files", path.split('/')[1]), ignore_errors=True)
 
@@ -97,7 +100,6 @@ def handle_message(update, context):
     if update.message:
 
         url = update.message.text
-        user = update.message.from_user.name
 
         if check_instagram(url):
             flag, post = get_insta_links(url)
@@ -111,26 +113,33 @@ def handle_message(update, context):
                 if flag:
 
                     media_group = []
-                    context.bot.send_message(update.message.chat.id, "Your data is loading ...")
+                    context.bot.send_message(update.message.chat.id,
+                                             "Your data is loading ...")
 
                     if len(contents):
                         for node in contents:
                             if node.is_video:
                                 # print(node.video_url)
-                                media_group.append(InputMediaVideo(node.video_url))
+                                media_group.append(
+                                    InputMediaVideo(node.video_url))
                             else:
-                                media_group.append(InputMediaPhoto(node.display_url))
-                        context.bot.sendMediaGroup(update.message.chat.id, media_group, timeout=200)
+                                media_group.append(
+                                    InputMediaPhoto(node.display_url))
+                        context.bot.sendMediaGroup(update.message.chat.id,
+                                                   media_group, timeout=200)
                     else:
                         if post.is_video:
 
-                            context.bot.sendMediaGroup(update.message.chat.id, [InputMediaVideo(post.video_url)],
+                            context.bot.sendMediaGroup(update.message.chat.id, [
+                                InputMediaVideo(post.video_url)],
                                                        timeout=200)
                         else:
-                            context.bot.sendMediaGroup(update.message.chat.id, [InputMediaPhoto(post.url)], timeout=200)
+                            context.bot.sendMediaGroup(update.message.chat.id, [
+                                InputMediaPhoto(post.url)], timeout=200)
 
                     if post.caption:
-                        context.bot.send_message(update.message.chat.id, post.caption)
+                        context.bot.send_message(update.message.chat.id,
+                                                 post.caption)
 
                     result = 1
                 else:
@@ -142,44 +151,6 @@ def handle_message(update, context):
                 context.bot.sendMessage(update.message.chat.id,
                                         "Invalid link. Check if the post is public.")
 
-        elif check_facebook(url):
-            flag, video_path, video_url = download_fb_video(url, "hd")
-            print(flag, video_path)
-
-            if flag:
-                video_file = open(video_path, 'rb')
-                context.bot.send_video(update.message.chat.id, video_file, timeout=200)
-                result = 1
-            else:
-                context.bot.sendMessage(update.message.chat.id,
-                                        "Invalid link. Check if the post is public.")
-                reason = 'Facebook error'
-        elif check_vkontakte(url):
-            flag, files, video_id = get_vk_resolutions(url)
-
-            if flag:
-
-                if len(files) > 0:
-
-                    keyboard = []
-
-                    for file in files:
-                        text = file.res.split('.')[0] + 'p (' + str(file.size) + ' MB)'
-                        callback_data = 'vk' + "--" + video_id + '--' + file.res + '--' + str(file.size)
-                        keyboard.append([InlineKeyboardButton(text=text, callback_data=callback_data)])
-
-                    reply_markup = InlineKeyboardMarkup(keyboard)
-
-                    context.bot.send_message(update.message.chat.id, text="Choose resolution",
-                                             reply_markup=reply_markup)
-                    result = 1
-                else:
-                    context.bot.send_message(update.message.chat.id,
-                                             text="I can’t download the video, there are several possible reasons: \n 1) The video is marked as 18+ \n 2) The video is live broadcast \n 3) The video is private \n 4) The video is uploaded to another hosting, for example, YouTube.")
-            else:
-                context.bot.sendMessage(update.message.chat.id,
-                                        "I can’t download the video, there are several possible reasons: \n 1) The video is marked as 18+ \n 2) The video is live broadcast \n 3) The video is private \n 4) The video is uploaded to another hosting, for example, YouTube.")
-
         elif check_youtube(url):
             flag, streams, video_id = get_youtube_resolutions(url)
 
@@ -188,13 +159,18 @@ def handle_message(update, context):
                 keyboard = []
 
                 for stream in streams:
-                    text = stream.res.split('.')[0] + ' (' + str(stream.size) + ' MB)'
-                    callback_data = "yt" + "--" + video_id + '--' + stream.res + '--' + str(stream.size)
-                    keyboard.append([InlineKeyboardButton(text=text, callback_data=callback_data)])
+                    text = stream.res.split('.')[0] + ' (' + str(
+                        stream.size) + ' MB)'
+                    callback_data = "yt" + "--" + video_id + '--' + stream.res + '--' + str(
+                        stream.size)
+                    keyboard.append([InlineKeyboardButton(text=text,
+                                                          callback_data=callback_data)])
 
                 reply_markup = InlineKeyboardMarkup(keyboard)
 
-                context.bot.send_message(update.message.chat.id, text="Choose resolution", reply_markup=reply_markup)
+                context.bot.send_message(update.message.chat.id,
+                                         text="Choose resolution",
+                                         reply_markup=reply_markup)
                 result = 1
             else:
                 context.bot.sendMessage(update.message.chat.id,
@@ -202,16 +178,20 @@ def handle_message(update, context):
                 reason = 'Youtube error'
 
         else:
-            context.bot.sendMessage(update.message.chat.id, "Invalid link. Check if the post is public.")
+            context.bot.sendMessage(update.message.chat.id,
+                                    "Invalid link. Check if the post is public.")
             reason = 'Invalid URL'
 
-        print(update.message.from_user.name, update.message.text, time.ctime(int(time.time())), result, reason,
+        print(update.message.from_user.name, update.message.text,
+              time.ctime(int(time.time())), result, reason,
               sep='    ', flush=True)
 
-        user_exist = db_users.search(query.user == update.message.from_user.name)
+        user_exist = db_users.search(
+            query.user == update.message.from_user.name)
 
         if len(user_exist) == 0:
-            db_users.insert({'user': update.message.from_user.name, 'chat_id': update.message.chat.id})
+            db_users.insert({'user': update.message.from_user.name,
+                             'chat_id': update.message.chat.id})
 
 
 if __name__ == '__main__':
--- utils.py
+++ utils.py
@@ -1,30 +1,25 @@
-import instaloader
-from instaloader import Post
 import os
 import random
+
+import instaloader
 import requests
-import re
-import urllib.request
 from bs4 import BeautifulSoup
-import vk
-from urllib.request import urlopen, urlretrieve
+from instaloader import Post
 from pytube import YouTube
 
-access_token = "VK TOKEN"
-session = vk.Session(access_token=access_token)
-api = vk.API(session)
-
-L = instaloader.Instaloader(sleep=True, download_geotags=False, filename_pattern="{shortcode}", quiet=False,
-                            download_video_thumbnails=False, download_comments=False)
-
-vk_video_reg = re.compile("-\d+_\d+")
+L = instaloader.Instaloader(sleep=True, download_geotags=False,
+                            filename_pattern="{shortcode}", quiet=False,
+                            download_video_thumbnails=False,
+                            download_comments=False)
 
 resolutions = ["144p", "240p", "360p", "480p", "720p", "1080p"]
 
+
 class Video_info:
-     def __init__(self, res, size):
-         self.res = res
-         self.size = size
+    def __init__(self, res, size):
+        self.res = res
+        self.size = size
+
 
 def check_instagram(url):
     if "instagram.com" in url:
@@ -33,48 +28,11 @@ def check_instagram(url):
         return False
 
 
-def check_facebook(url):
-    if "fb.com" in url or "facebook.com" in url:
-        return True
-    else:
-        return False
-
-
-def check_vkontakte(url):
-    if "vk.com" in url or "vkontakte.ru" in url:
-        return True
-    else:
-        return False
-
 def check_youtube(url):
     if "youtube.com" in url or "youtu.be" in url:
         return True
     else:
         return False
-
-
-def check_coub(url):
-    instareg = re.compile('(https?:\/\/www\.)?instagram\.com(\/p\/[a-zA-Z0-9-_]+\/?)')
-    if instareg.search(url) == None:
-        return False
-    return True
-
-
-def get_fb_link(html, quality):
-
-    if quality == "sd":
-        # Standard Definition video
-        url = re.search('sd_src:"(.+?)"', html)[0]
-    else:
-        # High Definition video
-        url = re.search('hd_src:"(.+?)"', html)[0]
-
-    # cleaning the url
-    url = url.replace('hd_src:"', '')
-    url = url.replace('sd_src:"', '')
-    url = url.replace('"', "")
-
-    return url
 
 
 def get_insta_shortcode(url):
@@ -87,61 +45,7 @@ def get_insta_shortcode(url):
     return shortcode
 
 
-def download_fb_video(url, quality):
-
-    try:
-        # Get source code
-        r = requests.get(url)
-
-        # Get download links
-        file_url = get_fb_link(r.text, quality)
-
-        name = str(random.random())[3:12] + ".mp4"
-        folder_name = str(random.random())[3:12] + "_facebook"
-        path = os.path.join("files", folder_name, name)
-
-        if not os.path.exists(os.path.join("files", folder_name)):
-            os.makedirs(os.path.join("files", folder_name))
-
-        urllib.request.urlretrieve(file_url, path)
-
-        return True, path, url
-    except Exception as e:
-        print(str(e))
-        return False, "", ""
-
-
-def download_instagram_content(url):
-
-    try:
-        shortcode = get_insta_shortcode(url)
-        # Generate random seed for non duplicate folders
-        # random_seed = random.randint(1,100)
-        # folder_name = shortcode + str(random_seed)
-        folder_name = str(random.random())[3:12] + "_instagram_" + shortcode
-
-        # Create folder for file
-        if not os.path.exists(folder_name):
-            os.makedirs(folder_name)
-
-        # Download content
-        post = Post.from_shortcode(L.context, shortcode)
-        a = L.download_post(post, target=os.path.join("files", folder_name))
-
-        # Get all content
-        relevant_path = os.path.join("files", folder_name)
-        included_extensions = ['jpg', 'mp4']
-        file_names = [os.path.join("files", folder_name, fn) for fn in os.listdir(relevant_path) if any(fn.endswith(ext) for ext in included_extensions)]
-
-        ## print(file_names)
-
-        return True, file_names
-
-    except AttributeError:
-        return False, []
-
 def get_insta_links(url):
-
     try:
         shortcode = get_insta_shortcode(url)
 
@@ -154,105 +58,7 @@ def get_insta_links(url):
         return False, []
 
 
-def get_vk_link(url):
-
-    pos_id = vk_video_reg.search(url)
-    video_id = url[pos_id.start():pos_id.end()]
-
-    a = api.video.get(videos=video_id, v="5.92")
-    player_url = a['items'][0]['player']
-
-    return player_url, video_id
-
-
-def get_vk_resolutions(url):
-
-    try:
-
-        player_url, video_id = get_vk_link(url)
-
-        #if check_youtube(player_url):
-        #    flag, files, video_id = get_youtube_resolutions(url)
-        #    return flag, 'yt', files, video_id
-
-        source = requests.get(player_url).text
-
-        parsed_html = BeautifulSoup(source)
-        tags = parsed_html.findAll('source')
-        urls = []
-
-        for tag in tags:
-            urls.append(tag.attrs['src'])
-
-        files = []
-
-        for res in ['1080.mp4', '720.mp4', '480.mp4', '360.mp4', '240.mp4']:
-            for url in urls:
-                if res in url:
-                    #resolutions.append(res)
-                    size = get_filesize_by_link(url)
-                    files.append(Video_info(res, size))
-                    #sizes.append(size)
-
-        return True, files, video_id
-
-    except Exception as e:
-        print(str(e))
-
-        return False, [], ""
-
-def download_vk_video(video_id, res):
-
-    a = api.video.get(videos=video_id, v="5.92")
-    player_url = a['items'][0]['player']
-
-    html = requests.get(player_url).text
-
-    parsed_html = BeautifulSoup(html)
-    tags = parsed_html.findAll('source')
-    urls = []
-
-    for tag in tags:
-        urls.append(tag.attrs['src'])
-
-    folder_name = str(random.random())[3:12] + "_vk_" + video_id
-
-    if not os.path.exists(os.path.join("files", folder_name)):
-        os.makedirs(os.path.join("files", folder_name))
-
-    for url in urls:
-        if res in url:
-            source = url.replace('\\/', '/')
-            reg = re.compile('/([^/]*\.mp4)')
-            name = reg.findall(source)[0]
-            path = os.path.join("files", folder_name, name)
-            urlretrieve(source, path)
-            break
-
-    return True, path
-
-
-def download_yt_video(video_id, res):
-
-    url = "https://youtu.be/" + video_id
-
-    yt = YouTube(url)
-
-    filename = video_id + '.mp4'
-    folder_name = str(random.random())[3:12] + "_yt_" + video_id
-    folder_path = os.path.join("files", folder_name)
-
-    if not os.path.exists(folder_path):
-        os.makedirs(folder_path)
-
-    filepath = yt.streams.filter(subtype='mp4', res=res, audio_codec='mp4a.40.2').first().download(output_path=os.path.join("files", folder_name),
-                                                                  filename=filename)
-
-    return True, filepath
-
-
 def get_youtube_resolutions(url):
-
     try:
 
         available_resolutions = []
@@ -263,9 +69,11 @@ def get_youtube_resolutions(url):
         video_id = yt.video_id
 
         for res in resolutions:
-            filter_streams = streams.filter(subtype='mp4', res=res, audio_codec='mp4a.40.2').all()
+            filter_streams = streams.filter(subtype='mp4', res=res,
+                                            audio_codec='mp4a.40.2').all()
             if len(filter_streams) > 0:
-                available_resolutions.append(Video_info(res=res, size=int(filter_streams[0].filesize / 1000000)))
+                available_resolutions.append(Video_info(res=res, size=int(
+                    filter_streams[0].filesize / 1000000)))
 
         return True, available_resolutions, video_id
 
@@ -275,52 +83,18 @@ def get_youtube_resolutions(url):
         return False, [], ""
 
 
-def get_filesize_by_link(url):
-    site = urlopen(url)
-    meta = site.info()
-
-    return round(int(meta["Content-Length"]) / 1000000, 2)
-
-
-def get_vk_link_by_res(video_id, res):
-    a = api.video.get(videos=video_id, v="5.92")
-    player_url = a['items'][0]['player']
-
-    html = requests.get(player_url).text
-
-    parsed_html = BeautifulSoup(html)
-    tags = parsed_html.findAll('source')
-    urls = []
-
-    for tag in tags:
-        urls.append(tag.attrs['src'])
-
-    folder_name = str(random.random())[3:12] + "_vk_" + video_id
-
-    if not os.path.exists(os.path.join("files", folder_name)):
-        os.makedirs(os.path.join("files", folder_name))
-
-    for url in urls:
-        if res in url:
-            source = url.replace('\\/', '/')
-            return source
-
-    return False
-
-
 def get_yt_link_by_res(video_id, res):
-
     url = "https://youtu.be/" + video_id
 
     yt = YouTube(url)
 
-    direct_url = yt.streams.filter(subtype='mp4', res=res, audio_codec='mp4a.40.2').first().url
+    direct_url = yt.streams.filter(subtype='mp4', res=res,
+                                   audio_codec='mp4a.40.2').first().url
 
     return direct_url
 
 
 def download_yt_video(video_id, res):
-
     url = "https://youtu.be/" + video_id
 
     yt = YouTube(url)
@@ -332,7 +106,9 @@ def download_yt_video(video_id, res):
     if not os.path.exists(folder_path):
         os.makedirs(folder_path)
 
-    filepath = yt.streams.filter(subtype='mp4', res=res, audio_codec='mp4a.40.2').first().download(output_path=os.path.join("files", folder_name),
-                                                                  filename=filename)
+    filepath = yt.streams.filter(subtype='mp4', res=res,
+                                 audio_codec='mp4a.40.2').first().download(
+        output_path=os.path.join("files", folder_name),
+        filename=filename)
 
     return True, filepath
